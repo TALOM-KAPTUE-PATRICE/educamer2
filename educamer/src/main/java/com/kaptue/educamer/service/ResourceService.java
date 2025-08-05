185dc1a4d9b09b0ecea218c267e0a4960905fa41
TALOM-KAPTUE-PATRICE <kaptuepatrice5@gmail.com>
2025-08-05 16:19:02 +0100

    fix(deploy): Correct database URL for Render production environment2

--- educamer/src/main/java/com/kaptue/educamer/service/ResourceService.java
+++ educamer/src/main/java/com/kaptue/educamer/service/ResourceService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @Service
 public class ResourceService {
 
